4a056bcf6520584668826e4f93222f6c48e57e75
shinelee <lzlasp@163.com>
2020-11-17 10:47:04 +0800

    add handler interface, update Encode interface, settings claass

--- thirdTestProject/Encoder.h
+++ thirdTestProject/Encoder.h
@@ -5,6 +5,7 @@
 
 class Encoder
 {
+public:
 	Encoder()
 	{
 
---
+++ thirdTestProject/Tag.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+enum class Tag
+{
+	Artist,
+	Title,
+	Genre,
+	Year,
+	Comment,
+	Track,
+	Version,
+	GainTrack,
+	GainAlbum,
+	Artwork
+};
+
+// Map a tag type to the UTF-8 encoded tag content.
+typedef std::map<Tag, std::string> Tags;
